10546a008f803bc4e7796ef6c840db1421267b50
Yefta Steven Marcellius <yeftasm@gmail.com>
2023-10-12 21:05:29 +0700

    Create table books

---
+++ database/migrations/2023_10_12_133626_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->char('isbn',13)->primary();
+            $table->string('judul')->nullable(false);
+            $table->integer('halaman')->default(0)->nullable(false);
+            $table->string('kategori')->default('uncategorized');
+            $table->string('penerbit')->nullable(false);
+            $table->timestamp('published_at');
+            $table->timestamp('updated_at');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title')-{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title')T1-2021130007-{{ config('app.name', 'Laravel') }}</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js','resources/css/app.css'])
 </head>
 
