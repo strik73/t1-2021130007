24480cb12e436ed02ab1c1da0db21422cbfa2255
Yefta Steven Marcellius <yeftasm@gmail.com>
2023-10-12 23:07:14 +0700

    Create seeder faker

---
+++ database/seeders/BookSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 25; $i++) {
+
+            $isbn = $faker->text(13);
+            $judul = $faker->sentence(5);
+            $halaman = $faker->numberBetween(0, 300);
+            $kategori = $faker->text(5);
+            $penerbit = $faker->sentence(3);
+            $created_at = $faker->dateTimeBetween('-3 months', 'now');
+
+            DB::table('books')->insert([
+                'isbn' => $isbn,
+                'judul' => $judul,
+                'halaman' => $halaman,
+                'kategori' => $kategori,
+                'penerbit' => $penerbit,
+                'created_at' => $created_at,
+                'updated_at' => $created_at,
+            ]);
+        }
+    }
+
+}
